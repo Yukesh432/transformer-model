f76494cd86d1d951dafd9be8fa182518c9433610
Yukesh432 <nsiwansh@gmail.com>
2023-07-23 14:12:30 +0545

    started input embedding

---
+++ transformer from scratch/input Embeddings.py
@@ -0,0 +1,9 @@
+# original sentence.....>> input Ids..........>>Embedding of 512
+import torch
+import torch.nn
+
+class InputEmbeddings(nn.Module):
+    def __init__(self, d_model: int, vocab_size: int):
+        super().__init__()
+        self.d_model= d_model
+        self.vocab_size= vocab_size
