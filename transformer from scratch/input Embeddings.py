f7ad6f2fb577022e31b75c1c0dcd373de120fc3b
Yukesh432 <nsiwansh@gmail.com>
2023-07-23 20:06:52 +0545

    complete input embeddings

--- transformer from scratch/input Embeddings.py
+++ transformer from scratch/input Embeddings.py
@@ -1,9 +1,16 @@
 # original sentence.....>> input Ids..........>>Embedding of 512
 import torch
-import torch.nn
+import torch.nn as nn
+import math
 
 class InputEmbeddings(nn.Module):
     def __init__(self, d_model: int, vocab_size: int):
         super().__init__()
         self.d_model= d_model
         self.vocab_size= vocab_size
+
+        #mapping betwn numbers to vector 
+        self.embedding= nn.Embedding(vocab_size, d_model)
+
+    def forward(self, x):
+        return self.embedding(x)* math.sqrt(self.d_model)
