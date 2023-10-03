affe30cb2e7d7019caa5d1f128e88775ea2e663a
Yukesh432 <nsiwansh@gmail.com>
2023-10-03 17:10:25 +0545

    update: multiheaded attention matrix initialize

--- transformer from scratch/transformer from scratch.py
+++ transformer from scratch/transformer from scratch.py
@@ -102,3 +102,16 @@ class FeedForwardBlock(nn.Module):
         return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
     
     
+# Multi-Headed Attention Block....
+
+class MultiHeadAttentionBlock(nn.Module):
+
+    def __init__(self, d_model:int, dropout:float) ->None:
+        super().__init__()
+        self.d_model= d_model
+        self.h= h
+        assert d_model % h == 0, "d_model is not divisible by h"
+        self.d_k= d_model // h
+        self.w_q= nn.Linear(d_model, d_model)
+        self.w_k= nn.Linear(d_model, d_model)
+        self.w_v= nn.Linear(d_model, d_model)
