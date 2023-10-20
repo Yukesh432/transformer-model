4d232a937016e4b8481dd9003be6df5cacb9b587
Yukesh432 <nsiwansh@gmail.com>
2023-10-20 23:30:45 +0545

    update: constructing encoder block

--- transformer from scratch/transformer from scratch.py
+++ transformer from scratch/transformer from scratch.py
@@ -167,6 +167,15 @@ class ResidualConnection(nn.Module):
 
 class EncoderBlock(nn.Module):
 
+    def __init__(self, self_attention_block: MultiHeadAttentionBlock, feed_forward_block: FeedForwardBlock, dropout: float)-> None:
+        super().__init__()
+        self.self_attention_block= self_attention_block
+        self.feed_forward_block= feed_forward_block
+        self.residual_connections= nn.ModuleList([ResidualConnection(dropout) for _ in range(2)])
+
+    def forward(self, x, src_mask):
+        
+
     
 
 
