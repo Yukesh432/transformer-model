3e71f4cfdbd81de4e3fd3b2d4f8165da7535f138
Yukesh432 <nsiwansh@gmail.com>
2023-11-18 23:26:08 +0545

    training

--- gpt2/finetuning-test.py
+++ gpt2/finetuning-test.py
@@ -11,25 +11,32 @@ def train_gpt2():
     # Load tokenizer and model, and send the model to the device
     tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
     tokenizer.pad_token = tokenizer.eos_token
+
     # model = GPT2LMHeadModel.from_pretrained('gpt2').to(device)
     model= GPT2ForSequenceClassification.from_pretrained('gpt2', num_labels=2).to(device)
 
-    texts= ['I am feeling good', 'this is not a coke', 'i watch old movies']
-    labels= [1,0,1]
+    # texts= ['I am feeling good', 'this is not a coke', 'i watch old movies']
+    # labels= [1,0,1]
 
     # dataset= load_dataset('text', data_files='v0.1_combined_transcripts.txt')['train']
+    dataset= load_dataset('imdb', split='train')
 
     def tokenize_function(examples):
         return tokenizer(examples["text"], padding='max_length', truncation=True, max_length=512)
 
-    dataset= Dataset.from_dict({'text': texts, 'labels': labels})
+    # dataset= Dataset.from_dict({'text': texts, 'labels': labels})
     tokenized_dataset = dataset.map(tokenize_function, batched=True)
     # tokenized_dataset = tokenized_dataset.remove_columns(['text'])
     # tokenized_dataset.set_format('torch', columns=['input_ids', 'attention_mask'])
-    tokenized_dataset.set_format('torch', columns=['input_ids', 'attention_mask', 'labels'])
+    tokenized_dataset.set_format('torch', columns=['input_ids', 'attention_mask', 'label'])
 
     # Define Data Collator
     # data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
+
+    # for evaluation data
+    eval_data= load_dataset('imdb', split='test')
+    eval_tokenized_data= dataset.map(tokenize_function, batched=True)
+    eval_tokenized_data.set_format('torch', columns=['input_ids', 'attention_mask', 'label'])
 
     # Training arguments
     training_args = TrainingArguments(
@@ -38,6 +45,9 @@ def train_gpt2():
         per_device_train_batch_size=1,  # Reduced batch size
         gradient_accumulation_steps=2,  # Gradient accumulation
         logging_dir='./logs',
+        load_best_model_at_end=True,
+        evaluation_strategy="epoch",
+        save_strategy="epoch",
     )
 
     # Initialize Trainer
@@ -45,12 +55,13 @@ def train_gpt2():
         model=model,
         args=training_args,
         train_dataset=tokenized_dataset,
+        eval_dataset=eval_tokenized_data
         # data_collator=data_collator,
     )
 
     # Train and save the model
     trainer.train()
-    model.save_pretrained('./conv_trained_model')
+    model.save_pretrained('./sentiment_trained_model')
 
 if __name__ == "__main__":
     
