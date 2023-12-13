4c3cdcf159604e966f656246c416fa1a70b403bf
Yukesh432 <nsiwansh@gmail.com>
2023-12-13 23:53:18 +0545

    ctrasnformer import not working

--- llama7b/test.py
+++ llama7b/test.py
@@ -6,12 +6,11 @@ from langchain.llms import CTransformers
 
 def getLLamaresponse(input_text,no_words,blog_style):
 
-    ### LLama2 model
-    llm=CTransformers(model='models/llama-2-7b-chat.ggmlv3.q8_0.bin',
-                      model_type='llama',
-                      config={'max_new_tokens':256,
-                              'temperature':0.01})
-    
+    # ### LLama2 model
+    llm=CTransformers(model='models/llama-2-7b-chat.ggmlv3.q8_0.bin', model_type='llama', config={'max_new_tokens':256, 'temperature':0.01})
+
+    # llm= CTransformers(model='marella/gpt-2-ggml', config={'max_new_tokens':256, 'temperature':0.01})
+    # llm = CTransformers(model="marella/gpt-2-ggml")
     ## Prompt Template
 
     template="""
