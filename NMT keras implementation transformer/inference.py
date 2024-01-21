07f6ee8131d07e241b3c233539aff313375cfb34
Yukesh432 <nsiwansh@gmail.com>
2024-01-21 19:56:04 +0545

    error

---
+++ NMT keras implementation transformer/inference.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+import tensorflow_text as text
+
+# Load the model
+model = tf.saved_model.load('translator')
+
+# Use the model
+# For example, to translate a Portuguese sentence to English
+pt_sentence = "Ola, como vai voce?"
+en_sentence = model(pt_sentence)
+
+print(f"Portuguese: {pt_sentence}")
+print(f"English: {en_sentence}")
