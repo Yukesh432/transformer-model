2552ee2c79516b6f29de6deb27ae2dbb2b052853
Yukesh432 <nsiwansh@gmail.com>
2024-02-04 23:31:57 +0545

    query change

--- NMT keras implementation transformer/inference.py
+++ NMT keras implementation transformer/inference.py
@@ -6,7 +6,7 @@ model = tf.saved_model.load('translator')
 
 # Use the model
 # For example, to translate a Portuguese sentence to English
-pt_sentence = "Ola, como "
+pt_sentence = "Ola
 en_sentence = model(pt_sentence)
 
 print(f"Portuguese: {pt_sentence}")
